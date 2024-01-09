7ce5a12a2dcb9b2e0fbe8885746d394ee6fd5aab
Praniyal Thapa <praniyalthapa100@gmail.com>
2024-01-09 16:57:42 +0545

    Completed api gateway service

--- index.js
+++ index.js
@@ -1,49 +1,4 @@
-// const express=require('express');
-// const app=express();
-// const morgan = require('morgan');
-// const axios=require('axios');
-// const rateLimit = require('express-rate-limit');
-// const { createProxyMiddleware } = require('http-proxy-middleware');
-// const PORT=3006;
 
-// const limiter = rateLimit({
-// 	windowMs: 15 * 60 * 1000, // 15 minutes
-// 	limit: 5, // Limit each IP to 5 requests per `window` (here, per 15 minutes).
-	
-// });
-
-//  app.use(morgan('combined'));
-//  app.use(limiter);
-//  app.use('/bookingservice', async (req, res, next) => {
-//     console.log(req.headers['x-access-token']);
-//     try {
-//         const response = await axios.get('http://localhost:3001/api/v1/isauthenticated', {
-//             headers: {
-//                 'x-access-token': req.headers['x-access-token']
-//             }
-//         });
-//         console.log(response.data);
-//         if(response.data.success) {
-//             next();
-//         } else {
-//             return res.status(401).json({
-//                 message: 'Unauthorised'
-//             })
-//         }
-//     } catch (error) {
-//         return res.status(401).json({
-//             message: 'Unauthorised'
-//         })
-//     }
-// })
-//  app.use('/bookingservice/api', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
-// app.get('/home',(req,res)=>{
-//     return res.json({message:'OK NOW'});
-// });
-
-// app.listen(PORT,()=>{
-//     console.log(`Server started at  port ${PORT}`);
-// });
 const express = require('express');
 const morgan = require('morgan');
 const { createProxyMiddleware } = require('http-proxy-middleware');
@@ -87,7 +42,7 @@ app.use('/bookingservice', createProxyMiddleware({ target: 'http://localhost:300
 app.get('/home', (req, res) => {
     return res.json({message: 'OK'});
 })
-
 app.listen(PORT, () => {
     console.log(`Server started at port ${PORT}`);
 });
+
